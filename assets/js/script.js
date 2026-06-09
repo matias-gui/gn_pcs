3f25ebdce46e2cb50d5802be72ea6e03e1bf7271
Guilherme Matias <guimatias890@gmail.com>
2026-06-08 22:17:59 -0300

    feat: Adicionado button whatsapp flutuante

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const buttonWhats = document.querySelector('.buttonWhats');
+
+buttonWhats.addEventListener('click', () => {
+    window.open('https://wa.me/5548991603808?text=Olá,%20gostaria%20de%20solicitar%20um%20orçamento.', '_blank')
+} )
