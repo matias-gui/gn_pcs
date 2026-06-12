8500f0d7408290e878cd61e62d3f3730c53514c3
Guilherme Matias <guimatias890@gmail.com>
2026-06-11 22:07:44 -0300

    feat: Navegação dos button do menu funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,28 @@ const buttonWhats = document.querySelector('.buttonWhats');
 const navBar = document.querySelector('.navBar');
 const buttonFecharMenu = document.querySelector('.buttonFecharMenu');
 const homeButton = document.querySelector('.homeButton');
+const buttonLinkMenu = document.querySelectorAll('.buttonLinkMenu');
+
 
 
 buttonMenu.addEventListener('click', () => {
     navBar.style.clipPath = 'circle(160% at 100% 0%)';
     navBar.style.transition = '1.2s all'
 })
-buttonFecharMenu.addEventListener('click', 
-    () => {
-        navBar.style.clipPath = 'circle(0% at 100% 0%)';
-        navBar.style.transition = '1.2s all'
-    }
-);
+
+function fecharMenu(){
+     navBar.style.clipPath = 'circle(0% at 100% 0%)';
+     navBar.style.transition = '1.2s all'
+}
+buttonFecharMenu.addEventListener('click', fecharMenu);
+
+buttonLinkMenu.forEach( button => {
+    button.addEventListener('click', fecharMenu);
+});
+
+
+
+
 
 
 
