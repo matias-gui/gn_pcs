aa0f0c6589dde497a75a0c2f43f3e023f8fe326a
Guilherme Matias <guimatias890@gmail.com>
2026-07-01 22:07:58 -0300

    feat: Formatação do estilo da seção Serviços Prestados

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,16 +21,11 @@ window.addEventListener("load", () => {
 buttonMenu.addEventListener('click', () => {
     navBar.style.clipPath = 'circle(160% at 100% 0%)';
     navBar.style.transition = '1.2s all';
-    carrosselServicosPrestados.style.opacity = '0';
-    carrosselServicosPrestados.style.transition = '1.2s all';
 });
 
 function fecharMenu(){
      navBar.style.clipPath = 'circle(0% at 100% 0%)';
      navBar.style.transition = '1.2s all';
-     carrosselServicosPrestados.style.opacity = '1';
-     carrosselServicosPrestados.style.transition = '1.2s all';
-    
 };
 
 buttonFecharMenu.addEventListener('click', fecharMenu);
@@ -50,33 +45,3 @@ function abrirWhatsapp(){
 };
 buttonWhats.addEventListener('click', abrirWhatsapp );
 homeButton.addEventListener('click',abrirWhatsapp );
- 
-
-
-const carrosselServicosPrestados = document.querySelector('.carrosselServicosPrestados');
-const servicosPrestadosItemImagem = document.querySelectorAll('.servicosPrestadosItemImagem');
-const buttonProximo = document.querySelector('#proximo');
-const buttonAnterior = document.querySelector('#anterior');
-let index = 0;
-
-function carrossel(){
-    carrosselServicosPrestados.style.transform = `translateX(-${index * 100}%)`
-}
-
-function proximo(){
-    index++;
-    if(index >= servicosPrestadosItemImagem.length ){
-        index = 0;
-    }
-    carrossel();
-}
-function anterior(){
-    index--;
-    if(index < 0){
-        index = servicosPrestadosItemImagem.length -1;
-    }
-    carrossel()
-}
-
-buttonProximo.addEventListener('click', proximo);
-buttonAnterior.addEventListener('click', anterior)
